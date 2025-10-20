9a240b148b2e070a9f7ed32704dc91fc9e7992c0
YagoGV <yagogv@gmail.com>
2025-10-20 23:49:47 +0200

    Selectores estilizados y funcionando

--- empleos.js
+++ empleos.js
@@ -1,3 +1,4 @@
+// Botones "Aplicar"
 const jobsListingSection = document.querySelector('.jobs-listing')
 
 jobsListingSection.addEventListener('click', function(event) {
@@ -8,4 +9,21 @@ jobsListingSection.addEventListener('click', function(event) {
         element.classList.add('is-applied')
         element.disabled = true
     }
+})
+
+// Filtros "Select"
+const filter = document.querySelector('#filter-location')
+const mensaje = document.querySelector('#filter-selected-value')
+const jobs = document.querySelectorAll('.job-listing-card')
+
+
+filter.addEventListener('change', function() {
+    const selectedValue = filter.value
+
+    jobs.forEach(job => {
+        const modalidad = job.dataset.modalidad
+        const isShown = selectedValue === '' || selectedValue === modalidad
+        job.classList.toggle('is-hidden', !isShown)
+    })
+    
 })
